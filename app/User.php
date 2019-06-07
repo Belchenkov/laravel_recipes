973c449cd30b960fe9de8ffad539e091219fd38b
Aleksey Belchenkov <u608110@gmail.com>
2019-06-07 06:14:28 +0300

    Customizing Mail Notice

--- app/User.php
+++ app/User.php
@@ -9,6 +9,16 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public function routeNotificationForNexmo()
+    {
+      return $this->phone;
+    }
+
+    public function routeNotificationForSlack()
+    {
+      return env('SLACK_WEBHOOK');
+    }
+
     /**
      * The attributes that are mass assignable.
      *
@@ -17,6 +27,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
+
+    protected $appends = array('thumbnail');
 
     /**
      * The attributes that should be hidden for arrays.
@@ -27,5 +39,20 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    public function questions()
+    {
+      return $this->hasMany('App\Question');
+    }
+
+    public function answers()
+    {
+      return $this->hasMany('App\Answer');
+    }
+
+    public function getThumbnailAttribute()
+    {
+      $path = pathinfo($this->profile_pic);
+      return $path['dirname'].'/'.$path['filename']."-thumb.jpg";
+    }
 
 }
