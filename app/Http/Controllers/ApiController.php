2f39886c491659c2b336cf23be401f1004964a67
Aleksey Belchenkov <u608110@gmail.com>
2019-06-15 06:28:21 +0300

    Get User Info from Github through Guzzle

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client as GuzzleClient;
 
 class ApiController extends Controller
 {
-    public function github()
+    public function github($username)
     {
-        
+        $client = new GuzzleClient();
+        $response = $client->get('https://api.github.com/users/' . $username);
+        $body = json_decode($response->getBody());
     }
 }
