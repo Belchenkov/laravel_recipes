70eba0f16967c6dfe8d739fb99b3f5a685202e11
Aleksey Belchenkov <u608110@gmail.com>
2019-06-15 07:08:33 +0300

    Get Weather using Coords

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -12,5 +12,37 @@ class ApiController extends Controller
         $client = new GuzzleClient();
         $response = $client->get('https://api.github.com/users/' . $username);
         $body = json_decode($response->getBody());
+        
+        return $body;
+    }
+
+    public function getWeather()
+    {
+        return view('weather');
+    }
+
+    public function postWeather(Request $request)
+    {
+        $this->validate($request, ['location' => 'required|min:5']);
+
+        // google api to get coords
+        $googleClient = new GuzzleClient();
+        $response = $googleClient->get('https://maps.googleapis.com/maps/api/geocode/json', [
+            'query' => [
+                'address' => $request->location
+            ]
+        ]);
+
+        $googleBody = json_decode($response->getBody());
+        $coords = $googleBody->results[0]->geometry->location;
+
+        // use the coords to get weather from darksky
+        $dsClient = new GuzzleClient();
+        $dsUrl = 'https://api.darksky.net/forecast/'.env('DARKSKY_API')."/$coords->lat,$coords->lng";
+        $dsResponse = $dsClient->get($dsUrl);
+        $weatherBody = json_decode($dsResponse->getBody());
+
+        return view('weather-ready')->with('weather', $weatherBody)->with('address', $googleBody->results[0]);
+
     }
 }
